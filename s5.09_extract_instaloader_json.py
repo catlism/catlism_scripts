d7bde6910f08584b22f97969a35eb534efe4f081
Matteo Di Cristofaro <deek85@gmail.com>
2023-07-15 16:48:50 +0200

    added licence header to s5.09

--- s5.09_extract_instaloader_json.py
+++ s5.09_extract_instaloader_json.py
@@ -1,3 +1,21 @@
+'''
+    Script to collect data from Instagram - part of the
+    Copyright (C) 2023 Matteo Di Cristofaro
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 # Import modules for: loading files using regular expression; reading JSON files; working with .xz compressed files;
 # working on local folders and files; using regular expressions; working with XML files
 import glob
